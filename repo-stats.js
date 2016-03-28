a8c740428b89e94821dd14b17b1ddc3a3395627a
Kathy Laird <kathy.laird@emc.com>
2016-03-28 07:07:01 -0600

    fix summary aggregate

--- repo-stats.js
+++ repo-stats.js
@@ -16,15 +16,127 @@ var getDbConnection = function () {
 
 var db = getDbConnection();
 
-var updatProjectWithCommits = async (function() {
-    var resultA = await (db.collection('commits').aggregate({$group: {_id: '$author.name', commitsByAuthor: { $sum: 1 }}}));
-    var resultB = await (db.collection('projects').findAndModify({
-        query: { key: 'BRM' },
-        update: { $set: { commits: resultA} },
-        new: true
-    }));
-    //console.log('updateResult' , resultB);
+var getSummaries = async(function () {
+    var userSummaries = await(db.collection('commits').aggregate(
+        {
+            "$group": {
+                "_id": {
+                    "user": "$author.name",
+                    "project": "$project.key"
+                },
+                "commitsByAuthorProject": {"$sum": 1}
+            }
+        },
+        {"$sort": {"commitsByAuthorProject": -1}},
+        {
+            "$group": {
+                "_id": "$_id.user",
+                "users": {
+                    "$push": {
+                        "project": "$_id.project",
+                        "count": "$commitsByAuthorProject"
+                    }
+                },
+                "count": {"$sum": "$commitsByAuthorProject"}
+            }
+        },
+        {"$sort": {"count": -1}}
+    ));
+
+    var projectSummaries = await(db.collection('commits').aggregate(
+        {
+            "$group": {
+                "_id": {
+                    "project": "$project.key",
+                    "user": "$author.name"
+                },
+                "commitsByProjectAuthor": {"$sum": 1}
+            }
+        },
+        {"$sort": {"commitsByProjectAuthor": -1}},
+        {
+            "$group": {
+                "_id": "$_id.project",
+                "users": {
+                    "$push": {
+                        "user": "$_id.user",
+                        "count": "$commitsByProjectAuthor"
+                    }
+                },
+                "count": {"$sum": "$commitsByProjectAuthor"}
+            }
+        },
+        {"$sort": {"count": -1}}
+    ));
+
+    var summaries = {
+        userSummaries: userSummaries,
+        projectSummaries: projectSummaries
+    };
+
+    //console.log('\n user summaries \n');
+    //var total = 0;
+    //_.each(summaries.userSummaries, function(summary) {
+    //    console.log(summary._id, summary.count);
+    //    total += summary.count;
+    //    _.each(summary.projects, function(project) {
+    //        console.log(project);
+    //    });
+    //});
+    //
+    //console.log('\n project summaries \n');
+    //_.each(summaries.projectSummaries, function(summary) {
+    //    console.log(summary._id, summary.count);
+    //    total += summary.count;
+    //    _.each(summary.users, function(user) {
+    //        console.log(user);
+    //    });
+    //});
+
+    // sample output
+    //user summaries
+    //
+    //Brian Reynolds 12
+    //dehru 5
+    //Jase 5
+    //klaird 5
+    //mturner 3
+    //Kris Thompson 2
+    //sjolat2 1
+    //
+    //project summaries
+    //
+    //SKUI 31
+    //{ user: 'Brian Reynolds', count: 12 }
+    //{ user: 'Jase', count: 5 }
+    //{ user: 'klaird', count: 5 }
+    //{ user: 'dehru', count: 3 }
+    //{ user: 'mturner', count: 3 }
+    //{ user: 'Kris Thompson', count: 2 }
+    //{ user: 'sjolat2', count: 1 }
+    //CUC 2
+    //{ user: 'dehru', count: 2 }
+
+    return summaries;
 });
 
-updatProjectWithCommits();
+//var updateProjectWithCommits = async(function () {
+    //var userSummary = await(db.collection('commits').aggregate({
+    //    $group: {
+    //        _id: '$author.name',
+    //        commitsByAuthor: {$sum: 1}
+    //    }
+    //}));
+    //console.log('userSummary', userSummary);
+    //
+    //var projectSummary = await(db.collection('commits').aggregate({
+    //    $group: {
+    //        _id: '$project.key',
+    //        commitsByProject: {$sum: 1}
+    //    }
+    //}));
+    //console.log('\n\nprojectSummary\n', projectSummary);
+//});
 
+
+getSummaries();
--- tally.js
+++ tally.js
@@ -4,6 +4,9 @@
 var request = require('superagent');
 var pmongo = require('promised-mongo');
 var _ = require('underscore');
+var await = require('asyncawait/await');
+var async = require('asyncawait/async');
+var moment = require('moment');
 
 /**
  * Create a db connection to the given db name.
@@ -15,87 +18,135 @@ var getDbConnection = function () {
 
 var db = getDbConnection();
 
-var baseUrl = 'http://ucas-stash.lss.emc.com/rest/api/1.0'
+var baseUrl = 'http://ucas-stash.lss.emc.com/rest/api/1.0';
 var projectsUrl = baseUrl + '/projects';
 var reposUrl = '/repos';
 var commitsUrl = '/commits';
 
-var clearDb = (function () {
-    return db.collection('projects').drop().then(function () {
-        return db.collection('repos').drop().then(function () {
-            return db.collection('commits').drop().then(function () {
-                console.log('db cleared');
+var createData = async (function() {
+    var dropDbs = [
+        db.collection('commits').drop(),
+        db.collection('repos').drop(),
+        db.collection('projects').drop()
+    ];
+    await (dropDbs);
+    console.log('clearDb done');
 
-            });
-        });
-    });
+    loadProjectsToMongo();
+});
 
-}());
+createData();
 
 var loadCommitsForRepo = function (repo) {
     if (!repo || !repo.project || !repo.project.link) {
         throw new Error('repo is not an object: ', repo);
         return;
     }
-    var url = baseUrl + repo.project.link.url + '/repos/' + repo.slug + '/commits';
-    console.log('URL IS: ', url);//console.log('URL IS: ', url);
-    request.get(url, function (err, res) {
+    var sevenDays = moment().subtract(7, 'd');
+
+    var commitsUrl = baseUrl + repo.project.link.url + '/repos/' + repo.slug + '/commits?limit=200&withCounts=true';
+    console.log('commitsUrl IS: ', commitsUrl);
+
+    request.get(commitsUrl, function (err, res) {
         if (err) throw err;
         var response = JSON.parse(res.text);
+        //  values; []
+        //  size: 25,
+        //  isLastPage: false,
+        //  start: 0,
+        //  limit: 25,
+        //  nextPageStart: 25,
+        //  authorCount: 31,
+        //  totalCount: 8792
+        //console.log('response.counts limit=', response.limit, 'totalCount=', response.totalCount);
+
         var commits = response.values;
-        commits.forEach(function (commit) {
+        console.log('find commits', commits.length);
+
+        //var earliestCommit = _.last(commits).authorTimestamp;
+        //console.log('earliestCommit=', earliestCommit);
+
+        var matchComments = _.filter(commits, function(commit) {
+            return commit.authorTimestamp >= sevenDays && commit.parents.length===1;
+        });
+        console.log('find matchComments', matchComments.length);
+
+        matchComments.forEach(function (commit) {
+            //console.log('commit.authorTimestamp=', commit.authorTimestamp);
+            commit.project = repo.project;
             commit.repo = repo.slug;
-            console.log(commit);
+            //console.log(commit);
             db.collection('commits').insert(commit).then(function (message) {
-                console.log('done', message);
+                //console.log('commits done', message);
             }).catch(function (err) {
-                console.log('error: ', err);
+                console.log('commits error: ', err);
             });
         });
 
     });
 };
+
+var INTERESTING_REPOS = [
+        'emc-ui', 'emc-web-sockets',
+        'emc-server-common',
+        'skyline-ui', 'skyline-ui-settings', 'skyline-ui-storage-assets', 'skyline-ui-protection'
+    ];
 
 var loadReposForProject = function (project) {
     if (!project || !project.link) {
         throw new Error('project is not an object: ', project);
         return;
     }
-    var url = baseUrl + project.link.url + reposUrl;
-    console.log('URL IS: ', url);
-    request.get(url, function (err, res) {
+    var reportsUrl = baseUrl + project.link.url + reposUrl;
+    console.log('reportsUrl IS: ', reportsUrl);
+
+    request.get(reportsUrl, function (err, res) {
         if (err) throw err;
         var response = JSON.parse(res.text);
         var repos = response.values;
-        repos = [_.last(repos)];//limiter
-        repos.forEach(function(repo) {
-            db.collection('repos').insert(repo).then(function (repo) {
-                console.log('done', repo);
-                loadCommitsForRepo(repo)
-            }).catch(function (err) {
-                console.log('error: ', err);
-            });
-        });
+        //repos = [_.last(repos)];//limiter // BRM - ucas
+        //repos = [repos[1]];//limiter SKUI - skyline-ui
 
+        repos.forEach(function(repo) {
+            if (_.contains(INTERESTING_REPOS, repo.name)) {
+                console.log('repo.name=', repo.name);
+                db.collection('repos').insert(repo).then(function (repo) {
+                    console.log('find repos', repos.length);
+                    loadCommitsForRepo(repo)
+                }).catch(function (err) {
+                    console.log('reposerror: ', err);
+                });
+            }
+        });
     });
 };
 
+// BRM CSC CUC DR EM HVP RFLR POL RAP SKY SKUI HVU WKSP CUC
+var INTERESTING_PROJECTS = ['CSC', 'CUC','SKUI'];
+
 var loadProjectsToMongo = function () {
+    console.log('projectsUrl is: ', projectsUrl);
+
     request.get(projectsUrl, function (err, res) {
         if (err) throw err;
         var response = JSON.parse(res.text);
         var projects = response.values;
-        projects = [_.first(projects)];//limiter
-        console.log('projects', projects);
+
+        console.log('find projects', projects.length);
+        //projects = [_.first(projects)];//limiter
+
         projects.forEach(function (project) {
-            db.collection('projects').insert(project).then(function (project) {
-                loadReposForProject(project);
-            }).catch(function (err) {
-                console.log('error: ', err);
-            });
+            if (_.contains(INTERESTING_PROJECTS, project.key)) {
+                console.log('project.key=', project.key);
+                db.collection('projects').insert(project).then(function (project) {
+                    loadReposForProject(project);
+                }).catch(function (err) {
+                    console.log('projects error: ', err);
+                });
+            }
         });
     });
 };
 
-loadProjectsToMongo();
+//loadProjectsToMongo();
 
